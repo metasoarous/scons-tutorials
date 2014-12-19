0454af2bbd1b9a30db08d03bbe5d25e08bb1449f
Christopher T Small <metasoarous@gmail.com>
2014-12-18 22:47:54 -0700

    Added script for installing Ubuntu prereqs

---
+++ install_ubuntu_prereqs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Mac not supported yet, but please add support if possible"
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo "Assuming you are running Ubuntu. Now installing via apt-get."
+  echo "You will need to enter your password to install things"
+  sudo apt-get install scons python-biopython python-matplotlib python-pip \
+    python-networkx muscle fasttree clustalw mafft graphviz 
+
+else
+  echo "Only OSX and Ubuntu supported at the moment, I'm afraid"
+fi
+
+sudo pip install colorbrewer graphviz
+
